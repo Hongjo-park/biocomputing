3e124574c4ae9852dbb95464bdc518cbc462e7b4
박홍조 <75837124+Hongjo-park@users.noreply.github.com>
2023-03-15 12:29:07 +0900

    Assingment_1_update

---
+++ src/assignments/Assignment1.py
@@ -0,0 +1,24 @@
+import re
+
+# convert all 'T's in the DNA sequence to 'U's
+# s : \n
+
+# If the input file does not have a DNA sequence, then print "No DNA sequence".
+
+# input file : case-insensitive ( upper or lower ) Ex. Attcg
+# t, T -> u, U
+
+if __name__ == '__main__':
+    # file open Ex. input.txt
+    # src/assignments/input.txt
+
+    f = open(input("파일 경로를 입력해주세요 (ex. ./input.txt ): "), 'r')
+    dna_sequence = ''.join([line.strip() for line in f.readlines()])
+    
+    if re.fullmatch('(A|T|C|G|a|t|c|g)*', dna_sequence) == None:
+        print("No DNA sequence.")
+    else:
+        # file output ( any file name ) Ex. output.txt
+        output = open("output.txt", "w")
+        output.write(dna_sequence)
+        output.close()
