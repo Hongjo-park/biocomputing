18507e41d9b2c768b2d61256e011dfea841ada3c
박홍조 <75837124+Hongjo-park@users.noreply.github.com>
2023-03-28 15:45:26 +0900

    update lower DNA sequence

--- src/assignments/Assignment2.py
+++ src/assignments/Assignment2.py
@@ -15,13 +15,13 @@ def is_dna_sequence(sequence: str) -> bool:
     '''
     dna sequence -> True / else -> False
     '''
-    return re.fullmatch('(A|T|C|G|a|t|c|g)*', sequence) != None
+    return re.fullmatch('(A|T|C|G|a|t|c|g)*', sequence) != None and len(sequence) >= 1
 
 def convert_dna_sequence(sequence: str) -> str:
     '''
     convert all 'A's, 'T's, 'C's, 'G's in the DNA sequence to 'T's, 'A's, 'G's, 'C's
     '''
-    return sequence.translate(str.maketrans('ATCG', 'TAGC'))
+    return sequence.translate(str.maketrans('ATCGatcg', 'TAGCtagc'))
 
 def output_reverse_complement(sequence: str, file_name='output.txt') -> None:
     output = open(file_name, "w")
